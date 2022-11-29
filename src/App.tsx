e9a7050f58d2f619da03ef71110cdd6944645510
Judzhin Miles <info@msbios.com>
2022-11-29 15:09:03 +0200

    Add Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { createElement as e } from 'react';
 
 function App() {
-    return (
-        <div>Hello World!</div>
-    );
+    // return (
+    //     <div>Hello World!!!!</div>
+    // );
+
+    // return React.createElement('h1', {}, 'Hello Element!!!');
+    return e('div', {className: 'container'}, [
+        e('h1', {className: 'font-bold'}, 'Test JSX'),
+        e('button', {className: ''}, 'Click me!')
+    ]);
 }
 
 export default App;
